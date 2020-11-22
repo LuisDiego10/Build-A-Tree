4c3d5f41480b87d19e58337d2f897bc6243ee51e
LuisDiego10 <66862458+LuisDiego10@users.noreply.github.com>
2020-11-22 01:24:13 -0600

    Make BST Tree
    Make BST Tree

---
+++ Trees/BST/src/BST.java
@@ -0,0 +1,62 @@
+public class BST {
+    Node root;
+
+    public BST (){
+        this.root = null;
+    }
+    void insertBST(int key){
+        root=insertRecursive(root,key);
+    }
+    Node insertRecursive(Node root,int key){
+        //tree is empty
+        if (root==null){
+            root=new Node(key);
+            return root;
+        }
+        //Traverse Tree
+        if (key<=root.key)     //insert in the left subtree
+            root.left=insertRecursive(root.left, key);
+        else if (key>=root.key)    //insert in the right subtree
+            root.right=insertRecursive(root.right, key);
+        // return pointer
+        return root;
+    }
+    void inorderBST() {
+        inorderRecursive(root);
+    }
+    void inorderRecursive(Node root){
+        if (root!=null){
+            inorderRecursive(root.left);
+            System.out.print(root.key+ " ");
+            inorderRecursive(root.right);
+        }
+    }
+    boolean searchBST(int key){
+        root=searchRecursive(root,key);
+        if (root!=null)
+            return true;
+        else
+            return false;
+    }
+    Node searchRecursive(Node root, int key){
+        if (root==null||root.key==key)
+            return root;
+        if (root.key>key)
+            return searchRecursive(root.left,key);
+        return searchRecursive(root.right,key);
+    }
+}
+class Main{
+    public static void main (String [] args){
+        BST firtsBST=new BST();
+        firtsBST.insertBST(45);
+        firtsBST.insertBST(10);
+        firtsBST.insertBST(7);
+        firtsBST.insertBST(12);
+        firtsBST.insertBST(90);
+        firtsBST.insertBST(50);
+        firtsBST.inorderBST();
+        boolean returnTF= firtsBST.searchBST(50);
+        System.out.println(returnTF);
+    }
+}
---
+++ Trees/BST/src/Node.java
@@ -0,0 +1,8 @@
+public class Node {
+    int key ;
+    Node left, right;
+    public Node (int item){
+        this.key=item;
+        left=right=null;
+    }
+}
