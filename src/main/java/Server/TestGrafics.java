7b2676b488bdc5a88711cf1450d3681fda222f69
isriom <62965230+isriom@users.noreply.github.com>
2020-12-07 20:52:11 -0600

    Btree bug fix
    Btree grafic
    
    Signed-off-by: isriom <62965230+isriom@users.noreply.github.com>

--- src/main/java/Server/TestGrafics.java
+++ src/main/java/Server/TestGrafics.java
@@ -1,11 +1,11 @@
 package Server;
 
 import AVL.AVL;
+import AVL.AVLNode;
 import BST.BST;
 import BST.NodeBST;
 import BTree.BTreeNode;
 import BTree.Btree;
-import AVL.AVLNode;
 import guru.nidi.graphviz.attribute.Font;
 import guru.nidi.graphviz.attribute.Rank;
 import guru.nidi.graphviz.attribute.Records;
@@ -13,16 +13,17 @@ import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.Graph;
 import guru.nidi.graphviz.model.MutableGraph;
-import guru.nidi.graphviz.model.MutableNode;
 import guru.nidi.graphviz.model.Node;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import static guru.nidi.graphviz.attribute.Rank.RankDir.TOP_TO_BOTTOM;
-import static guru.nidi.graphviz.attribute.Records.rec;
-import static guru.nidi.graphviz.model.Compass.*;
-import static guru.nidi.graphviz.model.Factory.*;
+
 import java.io.File;
 import java.io.IOException;
+
+import static guru.nidi.graphviz.attribute.Rank.RankDir.TOP_TO_BOTTOM;
+import static guru.nidi.graphviz.attribute.Records.rec;
+import static guru.nidi.graphviz.model.Compass.NORTH;
+import static guru.nidi.graphviz.model.Factory.*;
 
 public class TestGrafics extends Thread{
     private static Logger logger = LogManager.getLogger("Server");
@@ -32,39 +33,20 @@ public class TestGrafics extends Thread{
     public Graph ga;
 
     public static void main(String[] args) throws IOException {
+        Btree b = new Btree(2);
+        b.insert(10);
+        b.insert(20);
+        b.insert(5);
+        b.insert(8);
+        b.insert(12);
+        b.insert(30);
+        b.insert(7);
+        b.insert(17);
 
-        Node node0 = node("node0").with(Records.of(rec("f0", ""), rec("f1", "a"), rec("f2", "b"), rec("f3", "c"), rec("f4", "")));
-        Node node1 = node("node1").with(Records.of(rec("asd"), rec("v", "719"), rec("")));
-        Node node2 = node("node2").with(Records.of(rec("a1"), rec("805"), rec("p", "")));
-        Node node3 = node("node3").with(Records.of(rec("i9"), rec("718"), rec("")));
-        Node node4 = node("node4").with(Records.of(rec("e5"), rec("989"), rec("p", "")));
-        Node node5 = node("node5").with(Records.of(rec("t2"), rec("v", "959"), rec("")));
-        Node node6 = node("node6").with(Records.of(rec("o1"), rec("794"), rec("")));
-        Node node7 = node("node7").with(Records.of("s7", "659", ""));
-        System.out.print(node0.link(
-                between(port("f0"), node1.port("v", SOUTH)),
-                between(port("f1"), node2.port(WEST)),
-                between(port("f2"), node3.port(WEST)),
-                between(port("f3"), node4.port(WEST)),
-                between(port("f4"), node5.port("v", NORTH))));
-        MutableGraph g = mutGraph("example1").setDirected(true).add(
-
-                        node0.link(
-                                between(port("f0"), node1.port("v", SOUTH)),
-                                between(port("f1"), node2.port(WEST)),
-                                between(port("f2"), node3.port(WEST))),
-                        node0.link(
-                        between(port("f3"), node4.port(WEST)),
-                        between(port("f4"), node5.port("v", NORTH))));
-
-                g.add(
-                        node2.link(between(port("p"), node6.port(NORTH_WEST))),
-                        node4.link(between(port("p"), node7.port(SOUTH_WEST))));
-        try {
-            Graphviz.fromGraph(g).width(900).render(Format.PNG).toFile(new File("example/ex3.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        TestGrafics g= new TestGrafics();
+        g.playerTree=b;
+        g.player="isriom";
+        g.displayTree(b,"isriom");
 
     }
 
@@ -97,7 +79,6 @@ public class TestGrafics extends Thread{
             return createNodesAVL(((AVL)playerTree).root);
         }
         if (playerTree.getClass()== Btree.class){
-            ;
         }
         return createNodes(playerTree.root);
     }
@@ -152,31 +133,265 @@ public class TestGrafics extends Thread{
 
     }
 
-    public static void addBTreeDisplay(BST playertree){
-        if(((Btree)playertree).t==2){
-            auxAddBTreeDisplay2(((Btree)playertree).root);
-        }else if(((Btree)playertree).t==3){
-            auxAddBTreeDisplay3(((Btree)playertree).root);
+    public void addBTreeDisplay(BST playertree){
+        if (((Btree) playertree).t == 2) {
+            createBNode1(((Btree) playertree).root, 0);
+
+        } else if (((Btree) playertree).t == 3) {
+            createBNode2(((Btree) playertree).root, 0);
+        } else if (((Btree) playertree).t == 4) {
+            createBNode3(((Btree) playertree).root, 0);
         }
-        else if(((Btree)playertree).t==4){
-            auxAddBTreeDisplay4(((Btree)playertree).root);
+    }
+
+    public Node createBNode1(BTreeNode node, int num) {
+        String a = "";
+        String c = "";
+        String b = "";
+        try {
+            switch (node.n) {
+                case 0:
+                    break;
+                case 1:
+                    a = String.valueOf(node.keys[0]);
+                    break;
+                case 2:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    break;
+                case 3:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    break;
+            }
+        } catch (Exception e) {
+            logger.warn("keys not full or unknown error:"+e);
         }
-    }
-    public static void createBNode(BTreeNode node){
+        Node node0 = node("node" + num).with(Records.of(rec("f0", ""), rec("f1", a),
+                rec("f2", ""), rec("f3", b), rec("f4", ""), rec("f5", c),
+                rec("f6", "")));
+        gr.add(node0);
+        if(node.leaf){
+            return node0;
+        }
+        if(node.C[0]!=null) {
+            Node node1 = createBNode1(node.C[0], num + 1);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f0"), node1.port(NORTH))));
+        }
+        if(node.C[1]!=null) {
+            Node node1 = createBNode1(node.C[1], num + 150);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f2"), node1.port(NORTH))));
+        }
+        if(node.C[2]!=null) {
+            Node node1 = createBNode1(node.C[2], num + 300);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f4"), node1.port(NORTH))));
+        }
+        if (node.C[3] != null) {
+            Node node1 = createBNode1(node.C[3], num + 450);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f6"), node1.port(NORTH))));
+        }
+
+        return node0;
 
     }
 
-    public static void auxAddBTreeDisplay2(BTreeNode playertree){
+    public Node createBNode2(BTreeNode node, int num) {
+        String a = "";
+        String b = "";
+        String c = "";
+        String d = "";
+        String e = "";
+        try {
+            switch (node.n) {
+                case 0:
+                    break;
+                case 1:
+                    a = String.valueOf(node.keys[0]);
+                    break;
+                case 2:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    break;
+                case 3:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    break;
+                case 4:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    e = String.valueOf(node.keys[3]);
+                    break;
+                case 5:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    e = String.valueOf(node.keys[3]);
+                    d = String.valueOf(node.keys[4]);
+                    break;
+            }
+        } catch (Exception m) {
+            logger.warn("keys not full or unknown error:" + m);
+        }
+        Node node0 = node("node" + num).with(Records.of(
+                rec("f0", ""), rec("f1", a), rec("f2", ""), rec("f3", b),
+                rec("f4", ""), rec("f5", c), rec("f6", ""), rec("f7", d),
+                rec("f8", ""), rec("f9", e), rec("f10", "")));
+        gr.add(node0);
+        if (node.leaf) {
+            return node0;
+        }
+        if (node.C[0] != null) {
+            Node node1 = createBNode2(node.C[0], num + 1);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f0"), node1.port(NORTH))));
+        }
+        if (node.C[1] != null) {
+            Node node1 = createBNode2(node.C[1], num + 150);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f2"), node1.port(NORTH))));
+        }
+        if (node.C[2] != null) {
+            Node node1 = createBNode2(node.C[2], num + 300);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f4"), node1.port(NORTH))));
+        }
+        if (node.C[3] != null) {
+            Node node1 = createBNode2(node.C[3], num + 450);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f6"), node1.port(NORTH))));
+        }
+        if (node.C[4] != null) {
+            Node node1 = createBNode2(node.C[4], num + 600);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f8"), node1.port(NORTH))));
+        }
+        if (node.C[5] != null) {
+            Node node1 = createBNode2(node.C[5], num + 750);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f10"), node1.port(NORTH))));
+        }
+
+        return node0;
 
     }
 
-    public static void auxAddBTreeDisplay3(BTreeNode playertree){
+    public Node createBNode3(BTreeNode node, int num) {
+        String a = "";
+        String b = "";
+        String c = "";
+        String d = "";
+        String e = "";
+        String f = "";
+        String g = "";
+        try {
+            switch (node.n) {
+                case 0:
+                    break;
+                case 1:
+                    a = String.valueOf(node.keys[0]);
+                    break;
+                case 2:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    break;
+                case 3:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    break;
+                case 4:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    e = String.valueOf(node.keys[3]);
+                    break;
+                case 5:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    e = String.valueOf(node.keys[3]);
+                    d = String.valueOf(node.keys[4]);
+                    break;
+                case 6:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    e = String.valueOf(node.keys[3]);
+                    d = String.valueOf(node.keys[4]);
+                    f = String.valueOf(node.keys[4]);
+                    break;
+                case 7:
+                    a = String.valueOf(node.keys[0]);
+                    b = String.valueOf(node.keys[1]);
+                    c = String.valueOf(node.keys[2]);
+                    e = String.valueOf(node.keys[3]);
+                    d = String.valueOf(node.keys[4]);
+                    f = String.valueOf(node.keys[4]);
+                    g = String.valueOf(node.keys[4]);
+                    break;
+            }
+        } catch (Exception m) {
+            logger.warn("keys not full or unknown error:" + m);
+        }
+        Node node0 = node("node" + num).with(Records.of(
+                rec("f0", ""), rec("f1", a), rec("f2", ""), rec("f3", b),
+                rec("f4", ""), rec("f5", c), rec("f6", ""), rec("f7", d),
+                rec("f8", ""), rec("f9", e), rec("f10", ""), rec("f11", f)
+                , rec("f12", ""), rec("f13", g), rec("f14", "")));
+        gr.add(node0);
+        if (node.leaf) {
+            return node0;
+        }
+        if (node.C[0] != null) {
+            Node node1 = createBNode3(node.C[0], num + 1);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f0"), node1.port(NORTH))));
+        }
+        if (node.C[1] != null) {
+            Node node1 = createBNode3(node.C[1], num + 150);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f2"), node1.port(NORTH))));
+        }
+        if (node.C[2] != null) {
+            Node node1 = createBNode3(node.C[2], num + 300);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f4"), node1.port(NORTH))));
+        }
+        if (node.C[3] != null) {
+            Node node1 = createBNode3(node.C[3], num + 450);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f6"), node1.port(NORTH))));
+        }
+        if (node.C[4] != null) {
+            Node node1 = createBNode3(node.C[4], num + 600);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f8"), node1.port(NORTH))));
+        }
+        if (node.C[5] != null) {
+            Node node1 = createBNode3(node.C[5], num + 750);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f10"), node1.port(NORTH))));
+        }
+        if (node.C[6] != null) {
+            Node node1 = createBNode3(node.C[6], num + 900);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f12"), node1.port(NORTH))));
+        }
+        if (node.C[7] != null) {
+            Node node1 = createBNode3(node.C[7], num + 1050);
+            gr.add(node1);
+            gr.add(node0.link(between(port("f14"), node1.port(NORTH))));
+        }
+
+        return node0;
 
     }
-
-    public static void auxAddBTreeDisplay4(BTreeNode playertree){
-
-    }
-
 }
 
